ee333fe4d3d01b1a8338b7c7649bf37d3a44e634
Ian McInerney <Ian.S.McInerney@ieee.org>
2019-09-20 18:44:48 +0200

    Added documentation

--- filterevents.py
+++ filterevents.py
@@ -1,3 +1,21 @@
+'''
+This file contains the functions needed to perform stop filtering in LLDB.
+This filtering will only allow the target to stop execution if one of the criteria
+(filename matching, module name matching, function name matching) is satisfied.
+
+To use this script, it must first be imported to LLDB using this command:
+command script import filterevents.py
+
+Once it is imported, the filters can be added using the AddStopHookFilter function.
+This function takes the filters as arguments, and currently performs an OR across
+all three.
+An example filter to only stop the target if the source file contains "foo" anywhere
+in its name or path is:
+script filterevents.AddFilterHook( source_file="foo" )
+
+:author Ian McInerney
+:license MIT
+'''
 import argparse
 import lldb
 import re
@@ -5,6 +23,16 @@ import shlex
 
 
 def AddStopHookFilter( source_file=None, module_file=None, function=None ):
+    '''
+    Add a stop hook to the current target that filters stop events using the provided criteria.
+    If any of the regular expressions match, then the debugger stops. If none match, the debugger
+    continues and ignore the event.
+
+    :param source_file: regular expression to match against the source filename/path
+    :param module_file: regular expression to match against the compiled executable name
+    :param function: regular expression to match against the function name
+    '''
+
     # Create the arguments for the filter function
     filterArgs = ""
     if source_file != None:
@@ -22,6 +50,16 @@ def AddStopHookFilter( source_file=None, module_file=None, function=None ):
 
 
 def FilterEventStopHook( debugger, command, result, dict ):
+    '''
+    Command function that parses the current target information to see if the criteria are met
+    to stop the debugger or continue it.
+
+    This function takes several command line arguments to determine the criteria:
+    --source-file  - Regular expression to match against the source file name/path
+    --module-file  - Regular expression to match against the module file name/path
+    --function     - Regular expression to match against the function name
+    '''
+
     parser = argparse.ArgumentParser( "Only stop the debugger when criteria are met" )
 
     parser.add_argument( '--source-file', dest='srcFile',
